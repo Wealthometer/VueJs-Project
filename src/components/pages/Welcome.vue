4d802cce149b8450ee42b5ff9334501dcb5655b3
Wealthometer <wealthhajoh87@gmail.com>
2025-09-13 15:01:48 +0100

    Update

--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -3,7 +3,35 @@
 </script>
 
 <template>
+  <section id='welcome'>
+    <div class='benefits'>
+        <h2>Complete This Program<br/>If You Want To</h2>
 
+        <div>
+            <p>✅ Follow a simple program with proven result.</p>
+            <p>✅ Get fit, healthy, strong and absolutely shredded.</p>
+            <p>✅ Learn more about gym, training and technique.</p>
+        </div>
+
+        <div>
+            <h3>
+                The Training Plan
+            </h3>
+            <p>
+                Tis training plan follows a structure known as the
+                <strong>Bro Split</strong>.
+            </p>
+            <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i> &#x27F3;</h5>
+        </div>
+
+        <div class='card'>
+            <h3>&#128293; Your Challenge</h3>
+            <p>Complete all your workouts and track all your progress along the way...!!</p>
+            <p>Do You Accept ...?</p>
+            <button> Begin &rarr;</button>
+        </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
