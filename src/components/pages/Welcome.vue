92493979422a56337593ddb34b2719aa3179f6b6
Wealthometer <wealthhajoh87@gmail.com>
2025-09-13 15:40:11 +0100

    Update

--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -1,43 +1,37 @@
 <script setup>
-
+    defineProps({
+        handleChangeDisplay: Function
+    })
 </script>
 
 <template>
-  <section id='welcome'>
-    <div class='benefits'>
-        <h2>Complete This Program<br/>If You Want To</h2>
-
+    <section id="welcome">
+        <div class="benefits">
+            <h2>Complete this program<br/>if you want...</h2>
+            <div>
+                <p>✅ Follow a simple program with proven results.</p>
+                <p>✅ Get fit, healthy, strong and absolutely shredded.</p>
+                <p>✅ Learn more about gym, training and technique</p>
+            </div>
+        </div>
         <div>
-            <p>• Follow a simple program with proven result.</p>
-            <p>• Get fit, healthy, strong and absolutely shredded.</p>
-            <p>• Learn more about gym, training and technique.</p>
+            <h3>The Training Plan</h3>
+            <p>This training plan follows a structure known as the <strong>Bro Split</strong>.</p>
+            <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></h5>
         </div>
-
-        <div>
-            <h3>
-                The Training Plan
-            </h3>
-            <p>
-                Tis training plan follows a structure known as the
-                <strong>Bro Split</strong>.
-            </p>
-            <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i> &#x27F3;</h5>
+        <div class="card challenge">
+            <h3>🔥 Your Challenge</h3>
+            <p>Complete all the workouts and track your progress along the way!</p>
+            <p>Do you accept?</p>
+            <button @click="() => handleChangeDisplay(2)">Begin &rarr;</button>
         </div>
-
-        <div class='card challenge'>
-            <h3>&#128293; Your Challenge</h3>
-            <p>Complete all your workouts and track all your progress along the way...!!</p>
-            <p>Do You Accept ...?</p>
-            <button> Begin &rarr;</button>
-        </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <style scoped>
     #welcome,
     .challenge,
-    .benefits{
+    .benefits {
         display: flex;
         flex-direction: column;
     }
@@ -52,5 +46,16 @@
 
     .challenge {
         gap: 0.25rem;
+    }
+
+    @media (min-width: 640px) {
+        #welcome {
+            gap: 2rem;
+            padding: 1rem 0;
+        }
+
+        .benefits {
+            gap: 1rem;
+        }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import'./fanta.css'
-
+import './fanta.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
