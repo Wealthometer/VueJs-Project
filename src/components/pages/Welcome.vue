d88647ddbdbaa60ffa978e6fddd50194e28f9d94
Wealthometer <wealthhajoh87@gmail.com>
2025-09-13 15:28:55 +0100

    Update

--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -8,9 +8,9 @@
         <h2>Complete This Program<br/>If You Want To</h2>
 
         <div>
-            <p>✅ Follow a simple program with proven result.</p>
-            <p>✅ Get fit, healthy, strong and absolutely shredded.</p>
-            <p>✅ Learn more about gym, training and technique.</p>
+            <p>• Follow a simple program with proven result.</p>
+            <p>• Get fit, healthy, strong and absolutely shredded.</p>
+            <p>• Learn more about gym, training and technique.</p>
         </div>
 
         <div>
@@ -24,7 +24,7 @@
             <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i> &#x27F3;</h5>
         </div>
 
-        <div class='card'>
+        <div class='card challenge'>
             <h3>&#128293; Your Challenge</h3>
             <p>Complete all your workouts and track all your progress along the way...!!</p>
             <p>Do You Accept ...?</p>
@@ -35,5 +35,22 @@
 </template>
 
 <style scoped>
+    #welcome,
+    .challenge,
+    .benefits{
+        display: flex;
+        flex-direction: column;
+    }
 
+    #welcome {
+        gap: 1.5rem;
+    }
+
+    .benefits {
+        gap: 0.5rem;
+    }
+
+    .challenge {
+        gap: 0.25rem;
+    }
 </style>
